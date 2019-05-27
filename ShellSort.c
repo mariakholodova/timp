38a0c491ae11370c02f2f4d139d835808d5104ed
Maria Kholodova <47883948+mariakholodova@users.noreply.github.com>
2019-05-27 09:47:25 +0700

    файлы с выводом худших и лучших массивов

--- ShellSort.c
+++ ShellSort.c
@@ -30,48 +30,61 @@ void sorting(int* arr,size_t arr_len)
 
 int main(){
     srand(time(NULL));
-    printf("%10s %10s %10s %10s %10s %10s %10s %10s %10s\n","N","N*log(N)","N^2","N^3","time","swaps","cmps","best time","worst time");
+    printf("%10s %10s %10s %10s %10s %10s %10s %10s %10s\n","N","N*log(N)","N^2","N^3","time","swaps","compares","best time","worst time");
     int sizes[]={1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 50, 75, 100, 250, 500};
     for(int i=0;i<(sizeof(sizes)/sizeof(int));i++){
-        double res_time=0;
-        int res_swaps=0,res_compares=0;
+
+  		int* best_arr = malloc(sizeof(int)*sizes[i]);
+  		int* worst_arr = malloc(sizeof(int)*sizes[i]);
+
+        double res_time=0,best_time=-1,worst_time=-1;
+        int res_swaps=0,res_compares=0,worst_res_compares=0,best_res_compares=0,best_res_swaps,worst_res_swaps=0;
         for(int j=0;j<1000;j++){
-            swaps=0;
-        	compares=0;
             int* arr=malloc(sizeof(int)*sizes[i]);
-            for(int j=0;j<sizes[i];j++)
+            int* copy=malloc(sizeof(int)*sizes[i]);
+            for(int j=0;j<sizes[i];j++){
                 arr[j]=rand()%sizes[i];
+                copy[j]=arr[j];
+            }
             clock_t start_time=clock();
             sorting(arr,sizes[i]);
-            double now = (double)(clock()-start_time)/CLOCKS_PER_SEC;
-            res_time+=now;
             res_swaps+=swaps;
             res_compares+=compares;
+            double now = (double)(clock()-start_time)/CLOCKS_PER_SEC;
+            if(now<best_time || best_time<0){
+                best_time=now;
+                best_res_swaps=swaps;
+                best_res_compares=compares;
+                for(int k=0;k<sizes[i];k++){
+                	best_arr[k]=copy[k];
+                }
+            }
+            if(now>worst_time || worst_time<0){
+                worst_time=now;
+                worst_res_swaps=swaps;
+                worst_res_compares=compares;
+                for(int k=0;k<sizes[i];k++){
+                	worst_arr[k]=copy[k];
+                }
+            }
+            res_time+=now;
+            swaps=0;
+            compares=0;
             free(arr);
         }
-        
-        res_swaps/=1000;
-        res_compares/=1000;
-        res_time/=1000;
-        
-        int* best=malloc(sizeof(int)*sizes[i]);
-        int* worst=malloc(sizeof(int)*sizes[i]);
-        for(int j=0;j<sizes[i];j++){
-            best[j]=j;
-            worst[j]=sizes[i]-j;
-        }
-
-        clock_t start_time=clock();
-        sorting(best,sizes[i]);
-        double best_time = (double)(clock()-start_time)/CLOCKS_PER_SEC;
-        free(best);
-
-        start_time=clock();
-        sorting(worst,sizes[i]);
-        double worst_time = (double)(clock()-start_time)/CLOCKS_PER_SEC;
-        free(worst);
-
         printf("%10d %10d %10d %10d %10f %10d %10d %10f %10f\n", sizes[i],(int)(sizes[i]*log(sizes[i])),(int)pow(sizes[i],2),(int)pow(sizes[i],3),res_time,res_swaps,res_compares,best_time,worst_time);
+        if(sizes[i]==100){
+            printf("Best array:\n");
+            for(int j=0;j<sizes[i];j++){
+                printf("%d ", best_arr[j]);
+            }
+            printf("\n");
+            printf("Worst array:\n");
+            for(int j=0;j<sizes[i];j++){
+                printf("%d ", worst_arr[j]);
+            }
+            printf("\n");
+        }
     }
     return 0;
 }
