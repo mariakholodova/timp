8543e0638b3fb02a6eec3f9656e3af23ea8911dc
masha <mariakholodova@mail.ru>
2019-05-29 11:51:29 +0700

    path output

--- main.c
+++ main.c
@@ -92,10 +92,10 @@ int main(){
 
     int length = bruteforce(mas,n,stk,path);
     printf("%s", "Path: ");
-    printf("Length: %d\n", calc_length(mas,n,path));
 	for(int i=0;i<n;i++)
-        printf("%d ", path[i]);
+        	printf("%d ", path[i]);
 	printf("\n");
+    printf("Length: %d\n", calc_length(mas,n,path));
 
 	algo_floyd_vorshell(mas,n);
 	for(int i=0;i<n;i++){
